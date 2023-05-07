f35191b0fd0b65d4ca4ccc5ffad6eb56dd93e6d4
Francis1999S <seurafrancis99@gmail.com>
2023-05-07 10:53:26 -0300

    Agregado Header Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { MiComponente } from './components/mi-componente/mi-componente.component';
+import { HeaderComponent } from './components/Header/header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MiComponente
+    MiComponente,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
